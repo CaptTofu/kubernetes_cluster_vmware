fca5fdf83906830136cd4a89573f282b57b6829d
CaptTofu <patg@patg.net>
2015-03-25 09:13:02 -0400

    Fix to regex for password to allow "/"

--- bin/build_master.sh
+++ bin/build_master.sh
@@ -5,7 +5,7 @@ master_hostname=master
 pass=$(openssl passwd vmware) 
 
 cp ../coreos_image/coreos_production_vmware_image.vmdk ${master_hostname}_image.vmdk
-sed "s/PASSWD/${pass}/g;s/VM_HOST/${master_hostname}/g" ../master.yaml > ${master_hostname}_cloud_init.yaml
+sed "s|PASSWD|${pass}|g;s/VM_HOST/${master_hostname}/g" ../master.yaml > ${master_hostname}_cloud_init.yaml
 sed "s/VM_HOST/${master_hostname}/g;s|WORK_DIR|${work_dir}|g" ../vm_tmpl.vmx > ${master_hostname}.vmx
 mkdir -p /tmp/new-drive/openstack/latest
 cp ${master_hostname}_cloud_init.yaml /tmp/new-drive/openstack/latest/user_data
--- bin/build_nodes.sh
+++ bin/build_nodes.sh
@@ -10,7 +10,7 @@ do
   node_host=${node_prefix}${i}
   echo "Building node ${node_host}"
   cp ../coreos_image/coreos_production_vmware_image.vmdk ${node_host}_image.vmdk
-  sed "s/PASSWD/${pass}/g;s/VM_HOST/${node_host}/g;s/\<master-private-ip\>/$etcd_host/g" ../node.yaml > ${node_host}_cloud_init.yaml
+  sed "s|PASSWD|${pass}|g;s/VM_HOST/${node_host}/g;s/\<master-private-ip\>/$etcd_host/g" ../node.yaml > ${node_host}_cloud_init.yaml
   sed "s/VM_HOST/${node_host}/g;s|WORK_DIR|${work_dir}|g" ../vm_tmpl.vmx > ${node_host}.vmx
   mkdir -p /tmp/new-drive/openstack/latest
   cp ${node_host}_cloud_init.yaml /tmp/new-drive/openstack/latest/user_data
